57caadad8c92944ba8867ff211fd239b94775284
Josh Barrera <Josh Barrera>
2016-01-13 20:33:16 -0800

    Day 2 Assignment

---
+++ app.js
@@ -0,0 +1,14 @@
+//ask the user for input
+var firstname = prompt("What is your first name?");
+console.log(firstname)
+var lastname = prompt("What is your last name?");
+
+//tell the user something
+alert(lastname);
+
+//confirm a user’s action
+var choice = confirm("Confirm this for me real quick");
+console.log(choice)
+
+//tell the user something
+alert(choice);
